416310c40a90f8616784678f3a2c2771a322ac38
cecaseyUALR <38337932+cecaseyUALR@users.noreply.github.com>
2019-01-10 12:59:34 -0600

    Update FearlessFlapdoodle.cpp
    Moving description to readme

--- FearlessFlapdoodle.cpp
+++ FearlessFlapdoodle.cpp
@@ -2,17 +2,6 @@
 #define numsims 200000
 using namespace std;
 
-/*
-	This program uses random number generation to simulate the different
-	directions that Fearless Flapdoodle takes at random on an 10 x 10 area island.
-	The island houses a spring to replenish his thirst, an airport to take him home,
-	a cannibal village, giving both Flapdoodle and his cannibal captors a "taste" of fear.
-	
-	Other possible deaths include thirst (100 steps after last spring visit),
-	hunger (300 total steps without success), and drowning (going off the 10 x 10 grid).
-	In this program, I calculate the probability of each of Flapdoodle's possible
-	fates, and the average number of steps on a successful simulation.
-*/
 
 int main()
 {
